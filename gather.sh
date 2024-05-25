a54b4470cc7f0824897708568732ef0bfa58340b
Markus Heimerl <markusheimerl@hotmail.com>
2024-05-25 11:18:25 +0200

    🟢 .gitattributes 🟢 african_head_diffuse.tga 🟢 african_head_nm_tangent.tga 🟢 african_head_spec.tga 🟢 gather.sh 🟢 render3.py

---
+++ gather.sh
@@ -0,0 +1,4 @@
+wget "https://raw.githubusercontent.com/ssloy/tinyrenderer/79c0d0264a6869b900190804f70f64eec266b58e/obj/african_head/african_head.obj" -O "african_head.obj" -q
+wget "https://github.com/ssloy/tinyrenderer/blob/79c0d0264a6869b900190804f70f64eec266b58e/obj/african_head/african_head_diffuse.tga?raw=true" -O "african_head_diffuse.tga" -q
+wget "https://github.com/ssloy/tinyrenderer/blob/6d3acdb9ee452449b528ef3900ef5528dad0f807/obj/african_head/african_head_spec.tga?raw=true" -O "african_head_spec.tga" -q
+wget "https://github.com/ssloy/tinyrenderer/blob/6d3acdb9ee452449b528ef3900ef5528dad0f807/obj/african_head/african_head_nm_tangent.tga?raw=true" -O "african_head_nm_tangent.tga" -q
